9c5c5cba8607b60503bd48b03a8e6f36f86ecf76
Saeed Rahimi Manesh <Saeed Rahimi Manesh>
2016-12-14 20:41:37 +0330

    12/14/2016

--- task3/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['login'])){
-    unset($_SESSION['login']);
-    unset($_SESSION['username']);
-    unset($_SESSION['password']);
-    require_once "functions/functions.php";
-    redirect_to('index.php');
-}
